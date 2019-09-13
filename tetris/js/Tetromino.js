a701e13822cbbd983daee910e92e5bccd1d10d91
Edwin Svensson <git@olback.net>
2019-09-13 14:18:31 +0200

    refactor

--- tetris/js/Tetromino.js
+++ tetris/js/Tetromino.js
@@ -4,25 +4,100 @@ class Tetromino {
 
     /**
      *
-     * @param {Array.<number[]>} shape
-     * @param {Object} color
-     * @param {number} color.r
-     * @param {number} color.g
-     * @param {number} color.b
-     * 2d array describing the shape
-     * 1 is a square, 0 is space.
-     * Input should have this structure:
-     * [
-     *   [0, 0, 0, 0],
-     *   [0, 0, 0, 0],
-     *   [0, 0, 0, 0],
-     *   [0, 0, 0, 0]
-     * ]
+     * @param {string} shape
+     * Shapes: I, J, L, O, S, T, Z
      */
-    constructor(shape, color) {
-        this.shape2d = shape;
-        this.color = color;
-        this.shape1d = [].concat(...shape);
+    constructor(shape) {
+
+        // Shapes: I, J, L, O, S, T, Z
+        switch(shape) {
+
+            case 'I': {
+                this.shape2d = [
+                    [0, 1, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 1, 0, 0]
+                ];
+                this.color = { r: 0, g: 94, b: 94 };
+                break;
+            }
+
+            case 'J': {
+                this.shape2d = [
+                    [0, 0, 1, 0],
+                    [0, 0, 1, 0],
+                    [0, 1, 1, 0],
+                    [0, 0, 0, 0]
+                ];
+                this.color = { r: 0, g: 0, b: 94 };
+                break;
+            }
+
+
+            case 'L': {
+                this.shape2d = [
+                    [0, 1, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 1, 1, 0],
+                    [0, 0, 0, 0]
+                ];
+                this.color = { r: 94, g: 63, b: 0 };
+                break;
+            }
+
+            case 'O': {
+                this.shape2d = [
+                    [0, 1, 0, 0],
+                    [0, 1, 1, 0],
+                    [0, 1, 1, 0],
+                    [0, 0, 0, 0]
+                ];
+                this.color = { r: 94, g: 94, b: 0 };
+                break;
+            }
+
+            case 'S': {
+                this.shape2d = [
+                    [0, 1, 0, 0],
+                    [0, 1, 1, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 0]
+                ];
+                this.color = { r: 0, g: 94, b: 0 };
+                break;
+            }
+
+            case 'T': {
+                this.shape2d = [
+                    [0, 1, 0, 0],
+                    [0, 1, 1, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, 0, 0]
+                ];
+                this.color = { r: 63, g: 0, b: 94 };
+                break;
+            }
+
+            case 'Z': {
+                this.shape2d = [
+                    [0, 0, 1, 0],
+                    [0, 1, 1, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, 0, 0]
+                ];
+                this.color = { r: 94, g: 0, b: 0};
+                break;
+            }
+
+            default: {
+                throw new Error(`${shape} is not a valid shape`);
+            }
+
+        }
+
+        this.shape1d = [].concat(...this.shape2d);
+
     }
 
     // render() {
--- tetris/js/sketch.js
+++ tetris/js/sketch.js
@@ -1,48 +1,3 @@
-const tetrominos = {
-    I: new Tetromino([
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0]
-    ], { r: 0, g: 94, b: 94 }),
-    J: new Tetromino([
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0]
-    ], { r: 0, g: 0, b: 94 }),
-    L: new Tetromino([
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0]
-    ], { r: 94, g: 63, b: 0 }),
-    O: new Tetromino([
-        [0, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0]
-    ], { r: 94, g: 94, b: 0 }),
-    S: new Tetromino([
-        [0, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 0]
-    ], { r: 0, g: 94, b: 0 }),
-    T: new Tetromino([
-        [0, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 0, 0],
-        [0, 0, 0, 0]
-    ], { r: 63, g: 0, b: 94 }),
-    Z: new Tetromino([
-        [0, 0, 1, 0],
-        [0, 1, 1, 0],
-        [0, 1, 0, 0],
-        [0, 0, 0, 0]
-    ], { r: 94, g: 0, b: 0})
-}
-
 const size = {
     height: 20,
     width: 10,
@@ -70,9 +25,6 @@ function draw() {
     for (let y = 0; y < size.height; y++) {
         line(0, y * size.squareSize, size.width * size.squareSize, y * size.squareSize);
     }
-
-    
-
 
 }
 
